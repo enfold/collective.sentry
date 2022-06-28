6536f887fc01a85280f74426770cf5ada70101ca
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2022-06-28 22:09:58 +0000

    Preparing release 2.2.2

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(*rnames):
 
 
 setup(name='collective.sentry',
-      version='2.2.2.dev0',
+      version='2.2.2',
       description="Replace default Plone error_log to send data to GetSentry service",
       long_description=read('README.txt') + '\n',
       classifiers=[
