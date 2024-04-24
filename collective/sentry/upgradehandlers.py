7047720f56f70ad87f0e11ddd5b61de0a81f943e
Joshua LaPlace <joshua.laplace@enfoldsystems.com>
2024-04-24 15:50:05 -0500

    Merge js bundles with default plone bundle.

--- collective/sentry/upgradehandlers.py
+++ collective/sentry/upgradehandlers.py
@@ -1,5 +1,12 @@
+from collective.sentry.config import IS_PLONE_5
+
 
 def upgrade_default_from_1_to_2(portal_setup):
     portal_setup.runImportStepFromProfile('profile-collective.sentry:default',
             'skins')
 
+
+def upgrade_from_3_to_4(portal_setup):
+    if IS_PLONE_5:
+        profile = "profile-collective.sentry:plone5"
+        portal_setup.runImportStepFromProfile(profile, 'plone.app.registry')
